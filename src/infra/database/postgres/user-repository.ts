918f18df5b465a7d03c03d6319b2e445323ca764
Joel Filho <joelfilho02812@gmail.com>
2024-08-16 11:10:23 -0300

    feat: add find payer payee postgres repository

--- src/infra/database/postgres/user-repository.ts
+++ src/infra/database/postgres/user-repository.ts
@@ -1,11 +1,13 @@
-import { CheckBalanceRepository } from "@/data/protocols";
+import {
+    CheckBalanceRepository,
+    FindPayerPayeeByIdsRepository,
+} from "@/data/protocols";
 import { PgHelper } from "./helpers/connection";
 
-export class PgUserRepository implements CheckBalanceRepository {
-    async checkByUserId(
-        id: number,
-        value: number
-    ): Promise<CheckBalanceRepository.Result> {
+export class PgUserRepository
+    implements CheckBalanceRepository, FindPayerPayeeByIdsRepository
+{
+    async checkByUserId(id: number, val: number) {
         const query = `--sql
         SELECT 
             SUM(
@@ -17,7 +19,27 @@ export class PgUserRepository implements CheckBalanceRepository {
             ) > $2 AS payer_balance 
         FROM tb_transaction tt;`;
 
-        const result = await PgHelper.client?.query(query, [id, value]);
+        const result = await PgHelper.client?.query(query, [id, val]);
         return result?.rows[0].payer_balance as boolean;
+    }
+
+    async findPayerPayeeByIds(payerId: number, payeeId: number) {
+        const query = `--sql
+            WITH tb_joins AS (
+                SELECT tu.id as id FROM tb_transaction tt 
+                INNER JOIN tb_user tu ON tu.id = tt.payerid or tu.id = tt.payeeid 
+            )
+            
+            SELECT EXISTS (
+                SELECT id FROM tb_joins
+                WHERE id = 1
+            ) AND EXISTS (
+                SELECT id FROM tb_joins
+                WHERE id = 2
+            ) AS users_exists;
+        `;
+
+        const result = await PgHelper.client?.query(query, [payerId, payeeId]);
+        return result?.rows[0].users_exists;
     }
 }
