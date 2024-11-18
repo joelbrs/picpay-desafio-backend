d6c47703b3dbf89e8c795cbbce44543b1d99a81c
joelfilho <joelfilho@ciandt.com>
2024-11-18 19:26:07 -0300

    config: add cors

---
+++ src/main/java/br/com/joelf/picpay/infraestructure/configuration/CorsConfig.java
@@ -0,0 +1,28 @@
+package br.com.joelf.picpay.infraestructure.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${security.cors.allowed-origins}")
+    private String allowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer addCorsMappings() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedHeaders("Content-Type", "Accept")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(Boolean.TRUE);
+            }
+        };
+    }
+}
