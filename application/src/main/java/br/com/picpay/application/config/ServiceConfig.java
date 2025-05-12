bcd1faaf5ee3c3194a64ffb786aa8c3a0a294103
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2025-05-12 14:29:32 -0300

    chore: define transfer's service bean

---
+++ application/src/main/java/br/com/picpay/application/config/ServiceConfig.java
@@ -0,0 +1,22 @@
+package br.com.picpay.application.config;
+
+import br.com.picpay.factories.TransferServiceFactory;
+import br.com.picpay.ports.AccountRepository;
+import br.com.picpay.ports.TransferMessaging;
+import br.com.picpay.ports.TransferRepository;
+import br.com.picpay.service.TransferService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceConfig {
+
+    @Bean
+    public TransferService transferService(
+        AccountRepository accountRepository,
+        TransferRepository transferRepository,
+        TransferMessaging transferMessaging
+    ) {
+        return TransferServiceFactory.create(accountRepository, transferRepository, transferMessaging);
+    }
+}
