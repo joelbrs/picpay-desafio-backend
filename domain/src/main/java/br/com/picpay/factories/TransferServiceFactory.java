d67c0621a68503d7d6486e94d539163237c06be3
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2025-05-12 11:55:36 -0300

    feat: add transfer service factory

---
+++ domain/src/main/java/br/com/picpay/factories/TransferServiceFactory.java
@@ -0,0 +1,36 @@
+package br.com.picpay.factories;
+
+import br.com.picpay.ports.AccountRepository;
+import br.com.picpay.ports.TransferMessaging;
+import br.com.picpay.ports.TransferRepository;
+import br.com.picpay.service.TransferService;
+import br.com.picpay.service.validations.transfer.SufficientBalanceValidation;
+import br.com.picpay.service.validations.transfer.SuitableAccountTypeForTransfer;
+import br.com.picpay.validator.Validation;
+import br.com.picpay.validator.ValidationComposite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransferServiceFactory {
+
+    public static TransferService create(
+        AccountRepository accountRepository,
+        TransferRepository transferRepository,
+        TransferMessaging transferMessaging
+    ) {
+        Validation validation = createTransferValidation(accountRepository, transferRepository);
+        return new TransferService(validation, transferRepository, transferMessaging);
+    }
+
+    private static Validation createTransferValidation(
+        AccountRepository accountRepository,
+        TransferRepository transferRepository
+    ) {
+        List<Validation> createTransferValidations = new ArrayList<>();
+        createTransferValidations.add(new SuitableAccountTypeForTransfer(accountRepository));
+        createTransferValidations.add(new SufficientBalanceValidation(transferRepository));
+
+        return new ValidationComposite(createTransferValidations);
+    }
+}
