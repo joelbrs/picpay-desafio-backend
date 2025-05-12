b9fbe7ceb7609c3e8027b2b198338170650beff7
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2025-05-12 12:29:32 -0300

    chore: add jdbi

---
+++ application/src/main/java/br/com/picpay/application/config/RepositoryConfig.java
@@ -0,0 +1,34 @@
+package br.com.picpay.application.config;
+
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.spi.JdbiPlugin;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Configuration
+public class RepositoryConfig {
+
+    @Bean
+    public Jdbi jdbi(DataSource ds, List<JdbiPlugin> jdbiPlugins, List<RowMapper<?>> rowMappers) {
+        TransactionAwareDataSourceProxy proxy = new TransactionAwareDataSourceProxy(ds);
+        Jdbi jdbi = Jdbi.create(proxy);
+
+        jdbiPlugins.forEach(jdbi::installPlugin);
+        rowMappers.forEach(jdbi::registerRowMapper);
+
+        return jdbi;
+    }
+
+    @Bean
+    public JdbiPlugin jdbiPlugin() {
+        return new SqlObjectPlugin();
+    }
+}
